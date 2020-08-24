d3ca0a168b8da2078575a6ca21fd92094307478b
Cloudbop <boppincolin@gmail.com>
2020-08-24 18:53:34 +0100

    refactored game, see diff

--- scripts/game.js
+++ scripts/game.js
@@ -4,9 +4,7 @@
 window.game = {
   isAlive: (cell, neighbours) => {
 
-    if (Boolean(cell) && Number(neighbours) === 2) return 1
-
-    if (!Boolean(cell) && Number(neighbours) === 3) return 1
+    if (Boolean(cell) && Number(neighbours) === 2 || Number(neighbours) === 3) return 1
 
     return 0;
   }
