be37924f18eeed28abb9906c0ebe46c6ae3490dc
Cloudbop <boppincolin@gmail.com>
2020-08-24 17:54:42 +0100

    refactored filestructure for new module example test

---
+++ __tests__/example.spec.js
@@ -0,0 +1,9 @@
+require('../scripts/example');
+const example = window.example;
+//
+describe('An example module', () => {
+  test('should be defined', () => {
+    expect(example).toBeDefined();
+  })
+
+})
