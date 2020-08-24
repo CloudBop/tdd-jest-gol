25436d5d6f399fc3fdc62acafcfc9763156a1632
Cloudbop <boppincolin@gmail.com>
2020-08-24 19:12:32 +0100

    refactored game into arrow fns

--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,20 @@
 //
 // ACT
 //
+const isAlive = (cell, neighbours) => {
+
+  if (Boolean(cell) && Number(neighbours) === 2 || Number(neighbours) === 3) return 1
+
+  return 0;
+}
+const generate = (root) => new Array(root * root).fill(0)
+const regenerate = (grid) => grid.map(cell => {
+
+  return
+})
+
 window.game = {
-  isAlive: (cell, neighbours) => {
-
-    if (Boolean(cell) && Number(neighbours) === 2 || Number(neighbours) === 3) return 1
-
-    return 0;
-  },
-  generate: (root) => new Array(root * root).fill(0)
+  isAlive,
+  generate,
+  regenerate
 }
