836f49e0e4ad0f80e8216946592621c63aea62a7
Cloudbop <boppincolin@gmail.com>
2020-08-24 17:29:42 +0100

    fixed failing test

--- dictionary.js
+++ dictionary.js
@@ -1,10 +1,26 @@
+function splitStringBySpaces(string) {
+  // - all spaces 
+  return string.match(/[^ ]+/g)
+}
+
+
 module.exports = function dictionary(words) {
 
   //
   const result = {}
   //
   if (words) {
-    result[words] = 1;
+    const wordList = splitStringBySpaces(words)
+
+    wordList.forEach(word => {
+
+      if (result[word]) {
+        result[word]++
+      } else {
+        result[word] = 1;
+      }
+
+    });
   }
   //
   return result
