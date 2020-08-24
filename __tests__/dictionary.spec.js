a015da67fdd37ac528fcf030f768e4f46a9c04de
Cloudbop <boppincolin@gmail.com>
2020-08-24 17:16:39 +0100

    setup jest implemented first passing test

---
+++ __tests__/dictionary.spec.js
@@ -0,0 +1,9 @@
+const dictionary = require('../dictionary');
+//
+describe('dictionary', () => {
+  //
+  test('and empty string should return an empty object', () => {
+    //
+    expect(dictionary('')).toEqual({})
+  })
+})
