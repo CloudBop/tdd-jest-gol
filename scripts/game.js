7e235676e7837d5baf2e4f68bdc1f71380d0e2e8
Cloudbop <boppincolin@gmail.com>
2020-08-24 18:51:27 +0100

    passing test - live cell with 2 neighbours should return alive 1

--- scripts/game.js
+++ scripts/game.js
@@ -4,6 +4,8 @@
 window.game = {
   isAlive: (cell, neighbours) => {
 
+    if (Boolean(cell) && Number(neighbours) === 2) return 1
+
     if (!Boolean(cell) && Number(neighbours) === 3) return 1
 
     return 0;
